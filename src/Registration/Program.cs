ab600a1eae510d9c3245b5ca2508d65b91339a88
chris.condron <chris@condron.net>
2019-07-17 14:42:16 -0500

    switch to a subscription

--- src/Registration/Program.cs
+++ src/Registration/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 using System.Xml.Linq;
 using EventStore.ClientAPI;
 using EventStore.ClientAPI.SystemData;
@@ -19,56 +20,99 @@ namespace Registration
                 .SetDefaultUserCredentials(new UserCredentials("admin", "changeit"))
                 .KeepReconnecting()
                 .KeepRetrying()
-                .UseConsoleLogger()
+                //.UseConsoleLogger()
                 .Build();
 
             var conn = EventStoreConnection.Create(settings, IPEndPoint.Parse("127.0.0.1:1113"));
             conn.ConnectAsync().Wait();
 
             // Save an event 
-           
-            var @event = new UserRegistered(
-                Guid.NewGuid(),
+            var userId = Guid.NewGuid();
+            var registered = new UserRegistered(
+                userId,
                 "Mike",
                 "Jones",
                 "mike@aol.com");
 
-            
-            var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(@event));
-            
-            var eventData = new EventData(
-                                    Guid.NewGuid(), 
-                                    nameof(UserRegistered), 
-                                    true, 
-                                    data, 
-                                    null);
+            var namechange = new NameChanged(
+                userId,
+                "Micheal",
+                "Jones");
 
-            var stream = $"user-{@event.UserId}";
+
+            var eventData = new[]{
+                new EventData(
+                    Guid.NewGuid(),
+                    nameof(UserRegistered),
+                    true,
+                    Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(registered)),
+                    null),
+                new EventData(
+                    Guid.NewGuid(),
+                    nameof(NameChanged),
+                    true,
+                    Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(namechange)),
+                    null),
+            };
+
+            var stream = $"user-{userId}";
             conn.AppendToStreamAsync(stream, ExpectedVersion.NoStream, eventData).Wait();
             //create readmodel
 
-            var userRm = new RegisteredUsersRM();
+            Program.usersRm = new RegisteredUsersRM();
 
             //populate ReadModel
-            var registeredUsers = $"$et-{nameof(UserRegistered)}";
-            var slice = conn.ReadStreamEventsForwardAsync(registeredUsers, StreamPosition.Start, 100, true).Result;
-            
-            foreach (var evt in slice.Events)
-            {
+            var registeredUsers = "$ce-user";
+           // var slice = conn.ReadStreamEventsForwardAsync(registeredUsers, StreamPosition.Start, 100, true).Result;
+           var sub = conn.SubscribeToStreamFrom(registeredUsers,
+               null, CatchUpSubscriptionSettings.Default, 
+               gotUserEvent,
+               liveStarted);
 
-                var userRegistered =  JsonConvert.DeserializeObject(
-                    Encoding.UTF8.GetString(evt.Event.Data),
-                    typeof(UserRegistered))as UserRegistered;
-                userRm.Handle(userRegistered);
-                
-            }
-            //Update display
-            foreach (var user in userRm.Users){
-                Console.WriteLine($"Registered User: {user.FullName}");
-            }
+            
+           
 
             Console.WriteLine("press enter to exit");
             Console.ReadLine();
+        }
+
+        private static void liveStarted(EventStoreCatchUpSubscription obj){
+            //Update display
+            foreach (var user in Program.usersRm.Users.Values){
+                Console.WriteLine($"Registered User: {user.FullName}");
+            }
+
+            live = true;
+        }
+
+        private static RegisteredUsersRM usersRm;
+        private static bool live;
+        private static Task gotUserEvent(EventStoreCatchUpSubscription sub, ResolvedEvent evt)
+        {
+           
+            if (evt.Event.EventType == nameof(UserRegistered)){
+                var userEvent = JsonConvert.DeserializeObject(
+                    Encoding.UTF8.GetString(evt.Event.Data),
+                    typeof(UserRegistered)) as UserRegistered;
+                usersRm.Handle(userEvent);
+                if (live){
+                    Console.WriteLine($"Registered User: {usersRm.Users[userEvent.UserId].FullName}");
+                }
+            }
+            if (evt.Event.EventType == nameof(NameChanged)){
+                var userEvent = JsonConvert.DeserializeObject(
+                    Encoding.UTF8.GetString(evt.Event.Data),
+                    typeof(NameChanged)) as NameChanged;
+                usersRm.Handle(userEvent);
+                if (live){
+                    Console.WriteLine($"Registered User: {usersRm.Users[userEvent.UserId].FullName}");
+                }
+
+            }
+
+           
+
+            return Task.CompletedTask;
         }
     }
     public interface IMessage { }
@@ -79,16 +123,29 @@ namespace Registration
         void Handle(TEvent @event);
     }
     public class RegisteredUsersRM: 
-        IHandle<UserRegistered>{
-        public readonly List<User> Users = new List<User>();
+        IHandle<UserRegistered>,
+        IHandle<NameChanged>
+    {
+        public readonly Dictionary<Guid,User> Users = new Dictionary<Guid, User>();
 
         public void Handle(UserRegistered @event){
-           Users.Add( new User(@event.FirstName,@event.LastName));
-        }
+            try{
+                Users.Add(@event.UserId , new User(@event.FirstName,@event.LastName));
+            }
+            catch (Exception e){
+              //  Console.WriteLine(e);
+            }
+           
 
+        }
+        public void Handle(NameChanged @event){
+            var user = Users[@event.UserId];
+            user.Firstname = @event.FirstName;
+            user.Lastname = @event.LastName;
+        }
         public class User{
-            public string Firstname{ get; }
-            public string Lastname{ get; }
+            public string Firstname{ get; set; }
+            public string Lastname{ get; set; }
             public string FullName => $"{Lastname}, {Firstname}";
 
             public User(
@@ -98,6 +155,8 @@ namespace Registration
                 Lastname = lastname;
             }
         }
+
+      
     }
 
     /*
@@ -123,6 +182,21 @@ namespace Registration
             FirstName = firstName;
             LastName = lastName;
             Email = email;
+        }
+    }
+    public class NameChanged:IEvent
+    {
+        public readonly Guid UserId;
+        public readonly string FirstName;
+        public readonly string LastName;
+
+        public NameChanged(Guid userId,
+            string firstName,
+            string lastName)
+        {
+            UserId = userId;
+            FirstName = firstName;
+            LastName = lastName;
         }
     }
 }
