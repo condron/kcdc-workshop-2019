84bece794ced847623cfedce2bec4754579dde3b
chris.condron <chris@condron.net>
2019-07-17 13:36:47 -0500

    add simple read model direct to console

--- src/Registration/Program.cs
+++ src/Registration/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
+using System.Text;
+using System.Xml.Linq;
 using EventStore.ClientAPI;
 using EventStore.ClientAPI.SystemData;
+using Newtonsoft.Json;
 
 namespace Registration
 {
@@ -10,6 +14,7 @@ namespace Registration
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //Connect to Eventstore
             var settings = ConnectionSettings.Create()
                 .SetDefaultUserCredentials(new UserCredentials("admin", "changeit"))
                 .KeepReconnecting()
@@ -19,6 +24,43 @@ namespace Registration
 
             var conn = EventStoreConnection.Create(settings, IPEndPoint.Parse("127.0.0.1:1113"));
             conn.ConnectAsync().Wait();
+
+            // Save an event 
+           
+            var @event = new UserRegistered(
+                Guid.NewGuid(),
+                "Mike",
+                "Jones",
+                "mike@aol.com");
+
+            
+            var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(@event));
+            
+            var eventData = new EventData(
+                                    Guid.NewGuid(), 
+                                    nameof(UserRegistered), 
+                                    true, 
+                                    data, 
+                                    null);
+
+            var stream = $"user-{@event.UserId}";
+            conn.AppendToStreamAsync(stream, ExpectedVersion.NoStream, eventData).Wait();
+
+
+            var registeredUsers = $"$et-{nameof(UserRegistered)}";
+            var slice = conn.ReadStreamEventsForwardAsync(registeredUsers, StreamPosition.Start, 100, true).Result;
+            var events = new List<UserRegistered>();
+            foreach (var evt in slice.Events)
+
+            {
+                events.Add(  JsonConvert.DeserializeObject(
+                    Encoding.UTF8.GetString(evt.Event.Data),
+                    typeof(UserRegistered))as UserRegistered);
+            }
+
+            foreach (var userRegistered in events){
+                Console.WriteLine($"Registered User: {userRegistered.LastName},{userRegistered.FirstName}");
+            }
 
             Console.WriteLine("press enter to exit");
             Console.ReadLine();
@@ -41,7 +83,8 @@ namespace Registration
         public UserRegistered(Guid userId,
                               string firstName,
                               string lastName,
-                              string email){
+                              string email)
+        {
             UserId = userId;
             FirstName = firstName;
             LastName = lastName;
